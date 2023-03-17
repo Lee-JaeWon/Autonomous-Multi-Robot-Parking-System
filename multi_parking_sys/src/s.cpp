2b6385724213c7d3b462382939288f78b5144393
JooHyoSeok <sicparvismagna@naver.com>
2023-03-18 01:56:36 +0900

    함수화

--- multi_parking_sys/src/s.cpp
+++ multi_parking_sys/src/s.cpp
@@ -47,22 +47,40 @@
 #include <tf2/LinearMath/Quaternion.h>
 
 // Initialize ROS node
-
+geometry_msgs::Pose Quat2RPY2Quat(const geometry_msgs::ConstPtr& odom_pose,const geometry_msgs::ConstPtr& init_pose);
 
 // Create ROS node handle
-int cnt = 0;
+int cnt = 1;
 
-geometry_msgs::PoseStamped Pose_msg;
+geometry_msgs::PoseStamped odom_pose;
+geometry_msgs::PoseStamped odom_pose1;
+geometry_msgs::PoseStamped odom_pose2;
+
+
+
+void PoseCallback_0(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    odom_pose = *msg;
+}
+
+void PoseCallback_1(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    odom_pose1 = *msg;
+}
+
+void PoseCallback_2(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    odom_pose2 = *msg;
+}
+
 
 void initialPoseCallback_0(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
 {
-
+    // serviceCall(msg->pose.pose ,odom_pose.pose); // odom, init
+ 
     ROS_INFO("POSE_0 subscribed");
 
     // Create a service client for the start_trajectory service
     ros::NodeHandle nh;
     ros::ServiceClient Startclient = nh.serviceClient<cartographer_ros_msgs::StartTrajectory>("/start_trajectory");
-    ros::ServiceClient finishclient = nh.serviceClient<cartographer_ros_msgs::StartTrajectory>("/finish_trajectory");
+    ros::ServiceClient finishclient = nh.serviceClient<cartographer_ros_msgs::FinishTrajectory>("/finish_trajectory");
 
     // Create a StartTrajectoryRequest object and fill it with the necessary parameters
     cartographer_ros_msgs::StartTrajectory::Request startreq;
@@ -71,134 +89,78 @@ void initialPoseCallback_0(const geometry_msgs::PoseWithCovarianceStamped::Const
     cartographer_ros_msgs::StartTrajectory::Response startres;
     cartographer_ros_msgs::FinishTrajectory::Response finishres;
 
+    // create Pose msg 
+    geometry_msgs::Pose target_pose;
+
     finishreq.trajectory_id = cnt ; // int
-    if (finishclient.call(finishreq, finishres))
-        ROS_INFO("finish_trajectory service success..");
+    if (finishclient.call(finishreq, finishres)) ROS_INFO("finish_trajectory service success..");
 
     startreq.configuration_basename = "rplidar.lua"; // string
     startreq.configuration_directory="/home/lee-jaewon/catkin_ws/src/Autonomous-Multi-Robot-Parking-System/multi_parking_sys/lua"; // string
     startreq.use_initial_pose = true; // bool
 
-    geometry_msgs::Pose target_pose;
 
-    double odom_roll, odom_pitch, odom_yaw;
-    double init_roll, init_pitch, init_yaw;
-    tf::Quaterinion odom_quat(Pose_msg.pose.orientation.x,Pose_msg.pose.orientation.y,Pose_msg.pose.orientation.z,Pose_msg.pose.orientation.w)
-    tf::Matrix3x3(odom_quat).getRPY(odom_roll, odom_pitch, odom_yaw);
-    tf::Quaterinion init_quat(msg->pose.pose.orientation.x,msg->pose.pose.orientation.y,msg->pose.pose.orientation.z,Pose_msg.pose.orientation.w)
-    tf::Matrix3x3(init_quat).getRPY(init_roll, init_pitch, init_yaw);
+    // double odom_roll, odom_pitch, odom_yaw;
+    // double init_roll, init_pitch, init_yaw;
+
+    // //  odom_Quaternion to RPY
+    // tf::Quaternion odom_quat(odom_pose.pose.orientation.x,odom_pose.pose.orientation.y,odom_pose.pose.orientation.z,odom_pose.pose.orientation.w)
+    // tf::Matrix3x3(odom_quat).getRPY(odom_roll, odom_pitch, odom_yaw);
+
+    // // initialpose_Quaternion to RPY
+    // tf::Quaternion init_quat(msg->pose.pose.orientation.x,msg->pose.pose.orientation.y,msg->pose.pose.orientation.z,msg->pose.pose.orientation.w)
+    // tf::Matrix3x3(init_quat).getRPY(init_roll, init_pitch, init_yaw);
   
-    double target_roll,target_pitch,target_yaw;
+    // double target_roll,target_pitch,target_yaw;
 
-    target_pitch = odom_pitch - init_pitch;
-    target_roll = odom_roll - init_roll;
-    target_yaw = odom_yaw - init_yaw;
+    // target_pitch = init_pitch - odom_pitch ;
+    // target_roll  = init_roll  - odom_roll  ;
+    // target_yaw   = init_yaw   - odom_yaw ;
 
-    tf2::Quaternion target_quat;
-    target_quat.setRPY( target_pitch,target_roll,target_yaw ); 
+    // tf2::Quaternion target_quat;
+    // // target_RPY to Quaternion
+    // target_quat.setRPY( target_pitch, target_roll, target_yaw ); 
 
-    // msg->pose.pose
-    target_pose.position.x = msg->pose.pose.position.x - Pose_msg.pose.position.x
-    target_pose.position.y = msg->pose.pose.position.y - Pose_msg.pose.position.y
-    target_pose.position.z = 0 
+    // // msg->pose.pose
+    // target_pose.position.x = msg->pose.pose.position.x - odom_pose.pose.position.x
+    // target_pose.position.y = msg->pose.pose.position.y - odom_pose.pose.position.y
+    // target_pose.position.z = 0 
 
-    target_pose.orientation.x = target_quat.getX();
-    target_pose.orientation.y = target_quat.getY();
-    target_pose.orientation.z = target_quat.getZ();
-    target_pose.orientation.w = target_quat.getW();
+    // target_pose.orientation.x = target_quat.getX();
+    // target_pose.orientation.y = target_quat.getY();
+    // target_pose.orientation.z = target_quat.getZ();
+    // target_pose.orientation.w = target_quat.getW();
 
 
-    startreq.initial_pose = target_pose; // geometry _msgs
-
+    // startreq.initial_pose = target_pose; // geometry _msgs
+    startreq.initial_pose = Quat2RPY2Quat(odom_pose.pose, msg->pose.pose); // geometry _msgs
 
     startreq.relative_to_trajectory_id = cnt; /// int 
     // Call the start_trajectory service
-
-    if(Startclient.call(startreq, startres))
-        ROS_INFO("start_trajectory service success..");
+    if(Startclient.call(startreq, startres)) ROS_INFO("start_trajectory service success..");
 
     cnt ++;
 
 
 }
 
-
-void PoseCallback_0(const geometry_msgs::PoseStamped::ConstPtr& msg){
-    Pose_msg = *msg;
-}
 
 void initialPoseCallback_1(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
 {
-    ROS_INFO("POSE_1 subscribed");
-    // Create a service client for the start_trajectory service
-    ros::NodeHandle nh;
-    ros::ServiceClient Startclient = nh.serviceClient<cartographer_ros_msgs::StartTrajectory>("/start_trajectory");
-    ros::ServiceClient finishclient = nh.serviceClient<cartographer_ros_msgs::StartTrajectory>("/finish_trajectory");
-
-    // Create a StartTrajectoryRequest object and fill it with the necessary parameters
-    cartographer_ros_msgs::StartTrajectory::Request startreq;
-    cartographer_ros_msgs::FinishTrajectory::Request finishreq;
-
-    cartographer_ros_msgs::StartTrajectory::Response res;
-    cartographer_ros_msgs::StartTrajectory::Response finishres;
-
-    finishreq.trajectory_id = cnt ; // int
-    if (finishclient.call(finishreq, finishres))
-        ROS_INFO("finish_trajectory service success..");
-
-    startreq.configuration_basename = "rplidar.lua"; // string
-    startreq.configuration_directory="/home/lee-jaewon/catkin_ws/src/Autonomous-Multi-Robot-Parking-System/multi_parking_sys/lua"; // string
-    startreq.use_initial_pose = true; // bool
-    startreq.initial_pose = msg->pose.pose; // geometry _msgs
-    startreq.relative_to_trajectory_id = cnt; /// int 
-    // Call the start_trajectory service
-
-    if(Startclient.call(startreq, res))
-        ROS_INFO("start_trajectory service success..");
-
-    cnt ++;
+    serviceCall(msg->pose.pose ,odom_pose1.pose); // odom, init
 }
 
 void initialPoseCallback_2(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
 {
-    ROS_INFO("POSE_2 subscribed");
-
-    // Create a service client for the start_trajectory service
-    ros::NodeHandle nh;
-    ros::ServiceClient Startclient = nh.serviceClient<cartographer_ros_msgs::StartTrajectory>("/start_trajectory");
-    ros::ServiceClient finishclient = nh.serviceClient<cartographer_ros_msgs::StartTrajectory>("/finish_trajectory");
-
-    // Create a StartTrajectoryRequest object and fill it with the necessary parameters
-    cartographer_ros_msgs::StartTrajectory::Request startreq;
-    cartographer_ros_msgs::FinishTrajectory::Request finishreq;
-
-    cartographer_ros_msgs::StartTrajectory::Response res;
-    cartographer_ros_msgs::StartTrajectory::Response finishres;
-
-    finishreq.trajectory_id = cnt ; // int
-    if (finishclient.call(finishreq, finishres))
-        ROS_INFO("finish_trajectory service success..");
-
-    startreq.configuration_basename = "rplidar.lua"; // string
-    startreq.configuration_directory="/home/lee-jaewon/catkin_ws/src/Autonomous-Multi-Robot-Parking-System/multi_parking_sys/lua"; // string
-    startreq.use_initial_pose = true; // bool
-    startreq.initial_pose = msg->pose.pose; // geometry _msgs
-    startreq.relative_to_trajectory_id = cnt; /// int 
-    // Call the start_trajectory service
-
-    if(Startclient.call(startreq, res))
-        ROS_INFO("start_trajectory service success..");
-
-    cnt ++;
-
+    serviceCall(msg->pose.pose ,odom_pose2.pose); // odom, init
 }
+
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv,"initial_pose_subscriber");
     // Create subscriber to initial pose topic
     ros::NodeHandle nh;
-
     ros::Subscriber initialPoseSub_0 = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped>("/initialpose_0", 1, initialPoseCallback_0);
     ros::Subscriber initialPoseSub_1 = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped>("/initialpose_1", 1, initialPoseCallback_1);
     ros::Subscriber initialPoseSub_2 = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped>("/initialpose_2", 1, initialPoseCallback_2);
@@ -212,4 +174,80 @@ int main(int argc, char** argv)
     ros::spin();
 
     return 0;
+}
+
+geometry_msgs::Pose Quat2RPY2Quat(const geometry_msgs::ConstPtr& odom_msg,const geometry_msgs::ConstPtr& init_msg)
+{
+    // method that returns angle differences
+
+    double odom_roll, odom_pitch, odom_yaw;
+    double init_roll, init_pitch, init_yaw;
+
+    geometry_msgs::Pose target_pose;
+
+    //  odom_Quaternion to RPY
+    tf::Quaternion odom_quat(odom_msg->orientation.x,odom_msg->orientation.y,odom_msg->orientation.z,odom_msg->orientation.w)
+    tf::Matrix3x3(odom_quat).getRPY(odom_roll, odom_pitch, odom_yaw);
+
+    // initialpose_Quaternion to RPY
+    tf::Quaternion init_quat(init_msg->orientation.x,init_msg->orientation.y,init_msg->orientation.z,init_msg->orientation.w)
+    tf::Matrix3x3(init_quat).getRPY(init_roll, init_pitch, init_yaw);
+  
+    double target_roll,target_pitch,target_yaw;
+
+    target_roll  = init_roll  - odom_roll  ;
+    target_pitch = init_pitch - odom_pitch ;
+    target_yaw   = init_yaw   - odom_yaw ;
+
+    tf2::Quaternion target_quat;
+    // target_RPY to Quaternion
+    // target_quat.setRPY( target_pitch, target_roll, target_yaw ); 
+    target_quat.setRPY( 0, 0, target_yaw ); 
+
+    target_quat = target_quat.normalize();
+
+
+    // msg->pose.pose
+    target_pose.position.x = init_msg->position.x - odom_msg->position.x;
+    target_pose.position.y = init_msg->position.y - odom_msg->position.y;
+    target_pose.position.z = 0 ;
+
+    target_pose.orientation.x = target_quat.x();
+    target_pose.orientation.y = target_quat.y();
+    target_pose.orientation.z = target_quat.z();
+    target_pose.orientation.w = target_quat.w();    
+
+    return target_pose;
+}
+
+void serviceCall(const geometry_msgs::Pose::ConstPtr& odom_msg,const geometry_msgs::Pose::ConstPtr& init_msg)
+{
+    // Create a service client for the start_trajectory service
+    ros::NodeHandle nh;
+    ros::ServiceClient Startclient = nh.serviceClient<cartographer_ros_msgs::StartTrajectory>("/start_trajectory");
+    ros::ServiceClient finishclient = nh.serviceClient<cartographer_ros_msgs::FinishTrajectory>("/finish_trajectory");
+
+    // Create a StartTrajectoryRequest object and fill it with the necessary parameters
+    cartographer_ros_msgs::StartTrajectory::Request startreq;
+    cartographer_ros_msgs::FinishTrajectory::Request finishreq;
+
+    cartographer_ros_msgs::StartTrajectory::Response startres;
+    cartographer_ros_msgs::FinishTrajectory::Response finishres;
+
+    finishreq.trajectory_id = cnt ; // int
+    if (finishclient.call(finishreq, finishres)) ROS_INFO("finish_trajectory service success..");
+
+    startreq.configuration_basename = "rplidar.lua"; // string
+    startreq.configuration_directory="/home/lee-jaewon/catkin_ws/src/Autonomous-Multi-Robot-Parking-System/multi_parking_sys/lua"; // string
+    startreq.use_initial_pose = true; // bool
+    // startreq.initial_pose = target_pose; // geometry _msgs
+    startreq.initial_pose = Quat2RPY2Quat(odom_msg, init_msg); // geometry _msgs
+
+    startreq.relative_to_trajectory_id = cnt; /// int 
+    // Call the start_trajectory service
+    if(Startclient.call(startreq, startres)) ROS_INFO("start_trajectory service success..");
+
+    cnt ++;
+
+
 }
