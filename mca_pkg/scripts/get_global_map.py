[Edit]:numerical error fix

mca_pkg/scripts/get_global_map.py
@@ -30,7 +30,7 @@ class get_global_map():
 
         # ----------------------------------------
         # Data Topic
-        self.num_robots = 3
+        self.num_robots = 2
         self.map_topic = "/robot_1/map"
         self.robot_1_pose_ns = "/robot_1"
         self.robot_2_pose_ns = "/robot_2"
@@ -48,20 +48,34 @@ class get_global_map():
         rospy.Subscriber(self.robot_1_pose_ns + "/dp", PointStamped, self.waypointCallback, callback_args=0)
         rospy.Subscriber(self.robot_2_pose_ns + "/dp", PointStamped, self.waypointCallback, callback_args=1)
         rospy.Subscriber(self.robot_3_pose_ns + "/dp", PointStamped, self.waypointCallback, callback_args=2)
+        # Goal point
+        rospy.Subscriber(self.robot_1_pose_ns + "/move_base_simple/goal", PoseStamped, self.goalCallback, callback_args=0)
+        rospy.Subscriber(self.robot_2_pose_ns + "/move_base_simple/goal", PoseStamped, self.goalCallback, callback_args=1)
+        rospy.Subscriber(self.robot_3_pose_ns + "/move_base_simple/goal", PoseStamped, self.goalCallback, callback_args=2)
         
         # ----------------------------------------
         # Real data
         self.mapData = None
-        self.X = [[1.0, 1.0] for i in range(self.num_robots)]
-        self.P = [[1.0, 1.0] for i in range(self.num_robots)] 
-        self.PQ = [[1.0, 1.0, 1.0, 1.0] for i in range(self.num_robots)] 
-        self.WP = [[1.0, 1.0] for i in range(self.num_robots)] # way point from /robot_n/dp
-        self.euler = [[1.0, 1.0, 1.0] for i in range(self.num_robots)] 
+        self.X = [[1.0, 1.0] for _ in range(self.num_robots)]
+        self.RX = [[1.0, 1.0] for _ in range(self.num_robots)]
+        self.P = [[1.0, 1.0] for _ in range(self.num_robots)] 
+        self.PQ = [[1.0, 1.0, 1.0, 1.0] for _ in range(self.num_robots)] 
+        self.WP = [[1.0, 1.0] for _ in range(self.num_robots)] # way point from /robot_n/dp
+        self.RWP = [[1.0, 1.0] for _ in range(self.num_robots)] # way point from /robot_n/dp
+        self.euler = [[1.0, 1.0, 1.0] for _ in range(self.num_robots)]
+        self.V_des = [[0.0, 0.0] for _ in range(self.num_robots)]
+        self.goal = [[1.0, 1.0] for _ in range(self.num_robots)]
+        self.R_goal = [[100.0, 100.0] for _ in range(self.num_robots)]
+        self.vmax = [0.05 for _ in range(self.num_robots)]
+        self._yaw = 2
+
+        self.RWP_flag = True
 
         # ----------------------------------------
         self.ws_model = dict()
         self.ws_model['robot_radius'] = 3.5
         self.robot_patch = [None]*self.num_robots
+        self.goal_patch = [None]*self.num_robots
 
     # Map
     def mapCallback(self, data):
@@ -69,15 +83,16 @@ class get_global_map():
 
     # Pose Callback for each robot
     def poseCallback(self, data, robot_ns):
-        if self.X is not None:
-            for i in range(self.num_robots):
-                if robot_ns == i:
-                    self.X[i][0] = data.pose.position.x
-                    self.X[i][1] = data.pose.position.y
-                    self.PQ[i][0] = data.pose.orientation.x
-                    self.PQ[i][1] = data.pose.orientation.y
-                    self.PQ[i][2] = data.pose.orientation.z
-                    self.PQ[i][3] = data.pose.orientation.w
+        for i in range(self.num_robots):
+            if robot_ns == i:
+                # Position -> x,y
+                self.X[i][0] = data.pose.position.x
+                self.X[i][1] = data.pose.position.y
+                # Pose quaternion
+                self.PQ[i][0] = data.pose.orientation.x
+                self.PQ[i][1] = data.pose.orientation.y
+                self.PQ[i][2] = data.pose.orientation.z
+                self.PQ[i][3] = data.pose.orientation.w
 
     # DP Callback for each robot
     def waypointCallback(self, data, robot_ns):
@@ -85,6 +100,12 @@ class get_global_map():
             if robot_ns == i:
                 self.WP[i][0] = data.point.x
                 self.WP[i][1] = data.point.y
+
+    def goalCallback(self, data, robot_ns):
+        for i in range(self.num_robots):
+            if robot_ns == i:
+                self.goal[i][0] = data.pose.position.x
+                self.goal[i][1] = data.pose.position.y
 
     def get_data(self):
         figure = plt.figure()
@@ -105,10 +126,14 @@ class get_global_map():
                 self.map_starty = self.mapData.info.origin.position.y
 
                 for i in range(self.num_robots):
-                    self.X[i][0] = (self.X[i][0] - self.map_startx)/self.map_resolution
-                    self.X[i][1] = (self.X[i][1] - self.map_starty)/self.map_resolution
-                    self.WP[i][0] = (self.WP[i][0] - self.map_startx)/self.map_resolution
-                    self.WP[i][1] = (self.WP[i][1] - self.map_starty)/self.map_resolution
+                    self.RX[i][0] = (self.X[i][0] - self.map_startx)/self.map_resolution
+                    self.RX[i][1] = (self.X[i][1] - self.map_starty)/self.map_resolution
+                    self.RWP[i][0] = (self.WP[i][0] - self.map_startx)/self.map_resolution
+                    self.RWP[i][1] = (self.WP[i][1] - self.map_starty)/self.map_resolution
+
+                    self.R_goal[i][0] = (self.goal[i][0] - self.map_startx)/self.map_resolution
+                    self.R_goal[i][1] = (self.goal[i][1] - self.map_starty)/self.map_resolution
+
                     quaternion = (
                         self.PQ[i][0],
                         self.PQ[i][1],
@@ -117,11 +142,11 @@ class get_global_map():
                     )
                     self.euler[i] = euler_from_quaternion(quaternion)
 
-                    self.P[i][0] = cos(self.euler[i][2])
-                    self.P[i][1] = sin(self.euler[i][2])
+                    self.P[i][0] = cos(self.euler[i][self._yaw])
+                    self.P[i][1] = sin(self.euler[i][self._yaw])
 
                 for i in range(self.num_robots):
-                    self.robot_patch[i] = patches.Circle((self.X[i][0], self.X[i][1]),
+                    self.robot_patch[i] = patches.Circle((self.RX[i][0], self.RX[i][1]),
                                                          self.ws_model['robot_radius'],
                                                          facecolor=cmap(i),
                                                          edgecolor='black',
@@ -130,11 +155,21 @@ class get_global_map():
                                                          alpha=1,
                                                          zorder=2)
                     map_plt.add_patch(self.robot_patch[i])
-                    map_plt.arrow(self.X[i][0], self.X[i][1],
+                    map_plt.arrow(self.RX[i][0], self.RX[i][1],
                                   30*self.P[i][0],
                                   30*self.P[i][1],
                                   head_width=2.0, head_length=1.5, fc=cmap(i), ec=cmap(i))
+                    
+                    self.goal_patch[i] = patches.Rectangle((self.R_goal[i][0], self.R_goal[i][1]),
+                                                               width=4.0, height=4.0, color=cmap(i)
+                                                            )
+                    map_plt.add_patch(self.goal_patch[i])
                 img = np.zeros((self.map_height, self.map_width, 3), np.uint8)
+
+                self.V_des = self.compute_V_des(
+                        X=self.RX, way=self.RWP, goal=self.R_goal)
+                
+                print(self.V_des)
 
                 for i in range(0, self.map_height):
                     for j in range(0, self.map_width):
@@ -161,6 +196,35 @@ class get_global_map():
         def map_index_to_rgb_color(index):
             return scalar_map.to_rgba(index)
         return map_index_to_rgb_color
+    
+    def compute_V_des(self, X, way, goal):  # V desired
+        V_des = []
+        for i in range(self.num_robots):
+            dif_x = [way[i][k]-X[i][k] for k in range(2)]
+
+            norm = self.distance(dif_x, [0, 0])
+
+            norm_dif_x = [dif_x[k]*self.vmax[k] /
+                          norm for k in range(2)]  # Scaling
+
+            V_des.append(norm_dif_x[:])
+
+            # When the robot reach to final goal, robot stop.
+            if self.reach(X[i], goal[i], 2.5):
+                V_des[i][0] = 0.0
+                V_des[i][1] = 0.0
+        return V_des
+    
+    def reach(self, p1, p2, bound=0.5):
+        if self.distance(p1, p2) < bound:
+            return True
+        else:
+            return False
+        
+    def distance(self, pose1, pose2):
+        # compute Euclidean distance for 2D
+        return sqrt((pose1[0]-pose2[0])**2+(pose1[1]-pose2[1])**2)+0.001
+
 
 if __name__ == '__main__':
     _map_obj = get_global_map()