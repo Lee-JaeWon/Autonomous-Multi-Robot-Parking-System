bc7b36c7ece717834273d4ec6e999acb62e0d3e3
Lee-JaeWon <jawwoni@naver.com>
2023-05-30 21:43:45 +0900

    [Edit]:Pose to odom node

--- pose_to_odom/src/posetoodom.cpp
+++ pose_to_odom/src/posetoodom.cpp
@@ -3,15 +3,21 @@
 #include <nav_msgs/Odometry.h>
 #include <iostream>
 
-std::string s1;
+std::string s1 = "";
 ros::Publisher pub;
 nav_msgs::Odometry odom;
 
 void poseStampedCallback(const geometry_msgs::PoseStamped::ConstPtr &msg)
 {
-    odom.header = msg->header;
-    odom.pose.pose.position = msg->pose.position;
-    odom.pose.pose.orientation = msg->pose.orientation;
+    if(s1 != ""){
+        odom.header.seq = msg->header.seq;
+        odom.header.stamp = msg->header.stamp;
+        odom.header.frame_id = s1;
+
+        odom.pose.pose.position = msg->pose.position;
+        odom.pose.pose.orientation = msg->pose.orientation;
+    }
+    
 }
 
 int main(int argc, char **argv)
@@ -20,19 +26,18 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     ros::NodeHandle n("~");
 
-    if (ros::param::get("~namespace_one", s1))
+    if (ros::param::get("~namespace", s1))
     {
         ROS_INFO("pose_to_odom node got param: %s", s1.c_str());
-        s1 += '/';
     }
     else
         ROS_ERROR("pose_to_odom node Failed to get param '%s'", s1.c_str());
 
-    std::string pub_ns_one = "/" + s1 + "lio";
+    std::string pub_ns_one = "/" + s1 + "/pto";
     pub = nh.advertise<nav_msgs::Odometry>(pub_ns_one, 1000);
 
     // Subscribe to the pose topic
-    ros::Subscriber sub = nh.subscribe<geometry_msgs::PoseStamped>(s1 + "tracked_pose", 10, poseStampedCallback);
+    ros::Subscriber sub = nh.subscribe<geometry_msgs::PoseStamped>(s1 + "/tracked_pose", 10, poseStampedCallback);
 
     int hz = 1000;
     ros::Rate rate(hz);
